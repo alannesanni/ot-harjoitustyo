664202d04944edabb0b3871abadb047c1792cb07
Alanne Sanni M O <sanni.alanne@helsinki.fi>
2023-03-26 16:35:38 +0300

    viikko 2 tehtäviä

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,27 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_saldon_kasvattaminen_toimii(self):
+        self.maksukortti.lataa_rahaa(1500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 25.00 euroa")
+
+    def test_saldo_vähenee_oikein_kun_rahaa_on_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+
+
+    def test_saldo_ei_muutu_jos_ei_tarpeeksi_rahaa(self):
+        self.maksukortti.ota_rahaa(1500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_metodi_palauttaa_oikean_totuusarvon_true(self):
+        self.maksukortti.ota_rahaa(500)==True
+
+    
+    def test_metodi_palauttaa_oikean_totuusarvon_false(self):
+        self.maksukortti.ota_rahaa(1500)==False
+        
